2e525dddd9d61cce807b43bb1a4da83e0355e7e5
Joakoq12 <fullestnewyork@gmail.com>
2023-08-03 17:09:56 -0400

    Stage 1

--- src/components/todoApp.js
+++ src/components/todoApp.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 
 export default function TodoApp() {
     const [title, setTitle] = useState("INPUT");
+    const [todos, setTodos] = useState([]);
 
     function handleClick(e) {
         e.preventDefault();
@@ -14,9 +15,26 @@ export default function TodoApp() {
         setTitle(value);
     }
 
+     handleSubmit(e) {
+        e.preventDefault(); 
+
+        const newTodo = {
+            id: crypto.randomUUID(), 
+            title: title, 
+            completed: false
+        };
+
+        const temp = [... todos];
+        temp.unshift(newTodo);
+
+        setTodos(temp);
+  
+
+     }
+
     return (
       <div className="todoContainer">
-        <form Classname="todoCreateForm">
+        <form Classname="todoCreateForm" onSubmit={handleSubmit}>
           <input onChange={handleChange} className="todoInput" value={title} />
           <input
             onClick={handleClick}
@@ -29,4 +47,5 @@ export default function TodoApp() {
         </form>
       </div>
     );
+
 }
